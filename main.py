edb229d76fb112e8c1112178aebd3aba0fc6850d
pysalms <47726823+pysalms@users.noreply.github.com>
2026-06-06 20:13:13 +0000

    flask routing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,25 @@ app = Flask(__name__)
 def default():
     return f'Hello World'
 
+@app.route('/<name>')
+def hello(name):
+    return f'Hello {name}!'
+
+@app.route('/handle_params')
+def _handle_routes():
+    if "name" in request.args.keys() and "id" in request.args.keys():
+        id = int(request.args.get('id'))
+        name = request.args.get('name')
+        return f'Hey {name} you Id is {id}'
+    else:
+        return f'url not found'
+
+@app.route('/handle_methods', methods=['GET','POST'])
+def handle_methods():
+    if request.methods=='GET':
+        return f'it is a get request'
+    elif request.methods=="POST":
+        return f'it is post request'
 
 if (__name__ == '__main__'):
     app.run(host='0.0.0.0', debug=True)
